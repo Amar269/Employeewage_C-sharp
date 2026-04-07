9487aab4d3cf0267213b84b6e2f675c327fd6389
Amarnath Kolla <amarrama269@gmail.com>
2026-04-08 00:11:08 +0530

    updated the uc9 code

--- Employeewage/Employeewage/Program.cs
+++ Employeewage/Employeewage/Program.cs
@@ -4,33 +4,65 @@ class EmployeeWage
 {
     private string companyName;
     private int wagePerHour;
+    private int workingDays;
+    private int empType;
 
-    public EmployeeWage(string companyName, int wagePerHour)
+    public EmployeeWage(string companyName, int wagePerHour, int workingDays, int empType)
     {
         this.companyName = companyName;
         this.wagePerHour = wagePerHour;
+        this.workingDays = workingDays;
+        this.empType = empType;
     }
 
-    public int GetDailyHours()
-    {
-        Random rand = new Random();
-        int empCheck = rand.Next(0, 3);
 
-        return (empCheck == 1) ? 8 : (empCheck == 2 ? 4 : 0);
+    public void ComputeEmployeeWage()
+    {
+        int dailyHours = GetDailyHours();
+
+        int totalHours = workingDays * dailyHours;
+        int totalSalary = totalHours * wagePerHour;
+
+        Console.WriteLine("Company: " + companyName);
+        Console.WriteLine("Working Days: " + workingDays);
+        Console.WriteLine("Employee Type: " + (empType == 1 ? "Full Time" : "Part Time"));
+        Console.WriteLine("Total Hours: " + totalHours);
+        Console.WriteLine("Total Wage: " + totalSalary);
     }
 
-    public void ComputeWage()
+
+    public void ComputeWageWithLimit()
     {
+        int dailyHours = GetDailyHours();
+
         int totalHours = 0;
         int totalDays = 0;
 
         while (totalHours < 100 && totalDays < 20)
         {
             totalDays++;
-            totalHours += GetDailyHours();
+            totalHours += dailyHours;
         }
 
-        Console.WriteLine(companyName + " Wage: " + totalHours * wagePerHour);
+        int totalSalary = totalHours * wagePerHour;
+
+        Console.WriteLine("\n Monthly Wage by the Condition given ");
+        Console.WriteLine("Company: " + companyName);
+        Console.WriteLine("Employee Type: " + (empType == 1 ? "Full Time" : "Part Time"));
+        Console.WriteLine("Days Worked (limit): " + totalDays);
+        Console.WriteLine("Hours Worked (limit): " + totalHours);
+        Console.WriteLine("Total Wage (limit): " + totalSalary);
+    }
+
+
+    private int GetDailyHours()
+    {
+        switch (empType)
+        {
+            case 1: return 8;
+            case 2: return 4;
+            default: return 0;
+        }
     }
 }
 
@@ -38,10 +70,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        EmployeeWage emp1 = new EmployeeWage("TCS", 20);
-        EmployeeWage emp2 = new EmployeeWage("Infosys", 25);
+        EmployeeWage emp1 = new EmployeeWage("TCS", 20, 10, 1);
+        emp1.ComputeEmployeeWage();
+        emp1.ComputeWageWithLimit();
 
-        emp1.ComputeWage();
-        emp2.ComputeWage();
+        Console.WriteLine();
+
+        EmployeeWage emp2 = new EmployeeWage("Infosys", 20, 10, 2);
+        emp2.ComputeEmployeeWage();
+        emp2.ComputeWageWithLimit();
     }
 }
